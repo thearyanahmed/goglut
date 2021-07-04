6d252e111d07523a1f3b18030fb3a5f9d7c98dba
Aryan Ahmed Anik <thearyanahmed@gmail.com>
2021-07-05 03:18:53 +0600

    error /proc/self/exec not found

--- main.go
+++ main.go
@@ -2,84 +2,51 @@ package main
 
 import (
 	"fmt"
-	"golang.org/x/sys/unix"
 	"log"
-	"math/rand"
 	"os"
 	"os/exec"
-)
-
-const (
-	containerPath = "/var/run/glut/containers"
+	"syscall"
 )
 
 func main()  {
-
-	containers := make(map[string]bool)
-
 	fmt.Printf("[+] executing => \n%v\n",os.Args[2:])
 
 	switch os.Args[1] {
 	case "run":
-		containerID := createContainerID()
-
-		fmt.Printf("container %v\n",containerID)
-
-		containers[containerID] = true
-		run(containerID)
+		run()
+	case "child":
+		child()
 	default:
 		fmt.Printf("end of the line. am default\n")
 	}
 }
 
-func run (conID string) {
-	cmd := exec.Command(os.Args[2],os.Args[3:]...)
+func run () {
+	fmt.Printf("running %v as pid %v \n",os.Args[2],os.Getpid())
+
+	cmd := exec.Command("/proc/self/exec",append([]string{"child"},os.Args[2:]...)...)
 
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stdin
 	cmd.Stdin  = os.Stdin
 
-	createDirectoriesIfNotExists(conID)
-
-	cmd.SysProcAttr = &unix.SysProcAttr{
-		Chroot: getContanerMountPath(conID),
-	}
-	
 	must(cmd.Run())
 }
 
-func createContainerID() string {
-	randBytes := make([]byte, 6)
-	rand.Read(randBytes)
-	return fmt.Sprintf("%02x%02x%02x%02x%02x%02x",
-		randBytes[0], randBytes[1], randBytes[2],
-		randBytes[3], randBytes[4], randBytes[5])
-}
+func child () {
+	fmt.Printf("running %v as pid %v \n",os.Args[2],os.Getpid())
 
-func createDirectoriesIfNotExists(conID string)  {
-	homeDir := containerPath + "/" + conID
+	cmd := exec.Command(os.Args[2],os.Args[3:]...)
 
-	contDirs := []string{homeDir + "/fs", homeDir + "/fs/mnt", homeDir + "/fs/upperdir", homeDir + "/fs/workdir"}
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stdin
+	cmd.Stdin = os.Stdin
 
-	if err := createDirsIfDontExist(contDirs); err != nil {
-		log.Fatalf("Unable to create required directories: %v\n", err)
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
 	}
-}
 
-func getContanerMountPath(conID string) string {
-	return containerPath + "/" + conID + "/fs/mnt"
-}
-
-func createDirsIfDontExist(dirs []string) error {
-	for _, dir := range dirs {
-		if _, err := os.Stat(dir); os.IsNotExist(err) {
-			if err = os.MkdirAll(dir, 0755); err != nil {
-				log.Printf("Error creating directory: %v\n", err)
-				return err
-			}
-		}
-	}
-	return nil
+	must(cmd.Run())
 }
 
 func must(err error) {
